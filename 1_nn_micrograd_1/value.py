ab88a8a8e5315802680b0f237de46c19ba5039d7
yuro-py <zerone.taken@gmail.com>
2026-01-27 16:03:45 +0530

    Value object class progress : implemented callig, printing, adding, multiplying and computational graph(foundation for baktracking gradients)

--- 1_nn_micrograd_1/value.py
+++ 1_nn_micrograd_1/value.py
@@ -1,11 +1,25 @@
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=""):
         self.data = data
+        self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), "+")
+        return out
 
-a = Value(20)
-b = Value(-20)
-print(a + b)
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), "*")
+        return out
+
+
+a = Value(2.0)
+b = Value(-3.0)
+c = Value(10.0)
+d = (a * b) + c
+print(d)
+print(d._prev)
+print(d._op)
