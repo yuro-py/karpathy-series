50b7ec47889b112240bcf5907b592f391db932d7
yuro-py <zerone.taken@gmail.com>
2026-01-28 11:47:09 +0530

    implemented add, sub, mul, div, computational graph through children and operators. moving on to manual-backpropagation

--- 1_nn_micrograd_1/value.py
+++ 1_nn_micrograd_1/value.py
@@ -1,8 +1,10 @@
 class Value:
+
     def __init__(self, data, _children=(), _op=""):
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.grad = 0.0
 
     def __repr__(self):
         return f"Value(data={self.data})"
@@ -11,8 +13,16 @@ class Value:
         out = Value(self.data + other.data, (self, other), "+")
         return out
 
+    def __sub__(self, other):
+        out = Value(self.data - other.data, (self, other), "-")
+        return out
+
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), "*")
+        return out
+
+    def __div__(self, other):
+        out = Value(self.data / other.data, (self, other), "/")
         return out
 
 
@@ -20,6 +30,29 @@ a = Value(2.0)
 b = Value(-3.0)
 c = Value(10.0)
 d = (a * b) + c
-print(d)
-print(d._prev)
-print(d._op)
+f = Value(-2.0)
+L = (d * f).data
+
+
+def lol():
+
+    h = 0.0001
+    a = Value(2.0)
+    b = Value(-3.0)
+    c = Value(10.0)
+    d = (a * b) + c
+    f = Value(-2.0)
+    L1 = (d * f).data
+
+    a = Value(2.0 + h)
+    b = Value(-3.0)
+    c = Value(10.0)
+    d = (a * b) + c
+    f = Value(-2.0)
+    L2 = (d * f).data
+
+    slope = (L2 - L1) / h
+    print(slope)
+
+
+lol()
